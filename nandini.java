a44a58338d3cf222f547545805e5d424f4b2c766
NandiniMote <155938225+NandiniMote@users.noreply.github.com>
2024-04-22 19:10:13 +0530

    Update nandini.java

--- nandini.java
+++ nandini.java
@@ -1,28 +1,21 @@
-import java.util.Scanner;
-
 public class nandini {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number to check if it's prime: ");
-        int number = scanner.nextInt();
-        
-        if(isPrime(number))
+        int number = 12; // Check if 12 is prime
+
+        if (isPrime(number))
             System.out.println(number + " is a prime number.");
         else
             System.out.println(number + " is not a prime number.");
-        
-        scanner.close();
     }
-    
+
     // Function to check if a number is prime
     public static boolean isPrime(int num) {
-        if(num <= 1)
+        if (num <= 1)
             return false;
-        for(int i = 2; i <= Math.sqrt(num); i++) {
-            if(num % i == 0)
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0)
                 return false;
         }
         return true;
     }
 }
-
